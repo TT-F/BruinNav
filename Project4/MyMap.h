7510eab9dc0dd2b1e5492da103ab855bf9f8ecd9
TT-F <stevennm33@gmail.com>
2017-03-11 15:26:11 -0800

    leaf and root ? in associate helper first if version correct

--- Project4/MyMap.h
+++ Project4/MyMap.h
@@ -3,6 +3,12 @@
 // Skeleton for the MyMap class template.  You must implement the first six
 // member functions.
 
+#ifndef MyMap_h
+#define MyMap_h
+
+#include <iostream>
+
+
 template<typename KeyType, typename ValueType>
 class MyMap
 {
@@ -13,38 +19,39 @@ public:
 	int size() const;
 	void associate(const KeyType& key, const ValueType& value);
 
-	  // for a map that can't be modified, return a pointer to const ValueType
+	// for a map that can't be modified, return a pointer to const ValueType
 	const ValueType* find(const KeyType& key) const;
 
-	  // for a modifiable map, return a pointer to modifiable ValueType
+	// for a modifiable map, return a pointer to modifiable ValueType
 	ValueType* find(const KeyType& key)
 	{
 		return const_cast<ValueType*>(const_cast<const MyMap*>(this)->find(key));
 	}
 
-	  // C++11 syntax for preventing copying and assignment
+	// C++11 syntax for preventing copying and assignment
 	MyMap(const MyMap&) = delete;
 	MyMap& operator=(const MyMap&) = delete;
 
 private:
 	struct node
 	{
-		KeyType key;
-		ValueType value; 
+		KeyType m_key;
+		ValueType m_value;
 		node* left; //smaller 
 		node* right; //larger
 	};
-	node* root; 
+	node *root;
 	void destory_tree(node* leaf);
 	int size_helper(node* point);
-	ValueType* find_helper(const KeyType& key, node*leaf);
+	ValueType* find_helper(const KeyType& key, node*leaf) const;
 	void associate_helper(const KeyType & key, const ValueType & value, node* leaf);
+	//node* newNode(const KeyType& key, const ValueType& value);
 };
 
 template<typename KeyType, typename ValueType>
 inline MyMap<KeyType, ValueType>::MyMap()
 {
-	root = nullptr; 
+	root = nullptr;
 }
 
 template<typename KeyType, typename ValueType>
@@ -68,6 +75,14 @@ inline int MyMap<KeyType, ValueType>::size() const
 template<typename KeyType, typename ValueType>
 inline void MyMap<KeyType, ValueType>::associate(const KeyType & key, const ValueType & value)
 {
+	/*
+	if (root == nullptr)
+	{
+		std::cerr << "aa" << std::endl;
+		root = newNode(key, value);
+		return;
+	}
+	std::cerr << "aab" << std::endl;*/
 	associate_helper(key, value, root);
 }
 
@@ -83,10 +98,10 @@ inline void MyMap<KeyType, ValueType>::destory_tree(node * leaf)
 	if (leaf != nullptr)
 	{
 		destory_tree(leaf->left);
-		destory_tree(leaft->right);
+		destory_tree(leaf->right);
 		delete leaf;
 	}
-	return; 
+	return;
 }
 
 template<typename KeyType, typename ValueType>
@@ -99,20 +114,24 @@ inline int MyMap<KeyType, ValueType>::size_helper(node * point)
 		count += size_helper(point->right);
 		count++;
 	}
-	return count; 
+	return count;
 }
 
 template<typename KeyType, typename ValueType>
-inline ValueType * MyMap<KeyType, ValueType>::find_helper(const KeyType & key, node * leaf)
+inline ValueType * MyMap<KeyType, ValueType>::find_helper(const KeyType & key, node * leaf) const
 {
 	if (leaf != nullptr)
 	{
-		if (leaf->key < key) //if leaf's key is smaller than the key, go to right
+		if (leaf->m_key < key) //if leaf's key is smaller than the key, go to right
 			return find_helper(key, leaf->right);
-		else if (leaf->key > key) //if leaf's key is larger than the key, go to left 
+		else if (leaf->m_key > key) //if leaf's key is larger than the key, go to left 
 			return find_helper(key, leaf->left);
 		else
-			return leaf->value; 
+		{
+			ValueType* ptr;
+			ptr = &leaf->m_value;
+			return ptr;
+		}
 	}
 	return nullptr;
 }
@@ -120,36 +139,65 @@ inline ValueType * MyMap<KeyType, ValueType>::find_helper(const KeyType & key, n
 template<typename KeyType, typename ValueType>
 inline void MyMap<KeyType, ValueType>::associate_helper(const KeyType & key, const ValueType & value, node * leaf)
 {
-	if (find_helper(key, leaf) != nullptr) //the key exists 
-		find_helper(key, leaf)->value = value;  //replace existing value 
-	else
+
+	if (leaf == nullptr)
 	{
-		if (key < leaf->key)
+		//std::cerr << "aa" << std::endl;
+		root = new node; 
+		root->m_key = key;
+		root->m_value = value; 
+		root->right = nullptr;
+		root->left = nullptr;
+		//root = newNode(key, value);
+	}
+	else if (key == leaf->m_key)
+	{
+		//std::cerr << "b" << std::endl;
+		leaf->m_value = value;
+	}
+	else if (key < leaf->m_key)
+	{
+		//std::cerr << "c" << std::endl;
+		if (leaf->left != nullptr)
+			associate_helper(key, value, leaf->left);
+		else
 		{
-			if (leaf->left != nullptr)
-				associate_helper(key, value, leaf->left);
-			else
-			{
-				leaf->left = new node;
-				leaf->left->key = key;
-				leaf->left->value = value; 
-				leaf->left->left = nullptr;
-				leaf->left->right = nullptr;
-			}
+			leaf->left = new node;
+			leaf->left->m_key = key;
+			leaf->left->m_value = value;
+			leaf->left->left = nullptr;
+			leaf->left->right = nullptr;
 		}
-		else if (key > leaf->key)
+	}
+	else if (key > leaf->m_key)
+	{
+		//std::cerr << "d" << std::endl;
+		if (leaf->right != nullptr)
+			associate_helper(key, value, leaf->right);
+		else
 		{
-			if (leaf->right != nullptr)
-				associate_helper(key, value, leaf->right);
-			else
-			{
-				leaf->right = new node; 
-				leaf->right->key = key;
-				leaf->right->value = value; 
-				leaf->right->left = nullptr; 
-				leaf->right->right = nullptr;
-			}
+			leaf->right = new node;
+			leaf->right->m_key = key;
+			leaf->right->m_value = value;
+			leaf->right->left = nullptr;
+			leaf->right->right = nullptr;
 		}
 	}
 }
+//
+//template<typename KeyType, typename ValueType>
+//typename MyMap<KeyType, ValueType>::node * MyMap<KeyType, ValueType>::newNode(const KeyType & key, const ValueType & value)
+//{
+//	node* k = new node;
+//	k->m_key = key;
+//	k->m_value = value;
+//	k->left = nullptr;
+//	k->right = nullptr;
+//	return k;
+//}
+
+
+
+#endif MyMap_h
+
 
